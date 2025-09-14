63c7238b34ca331f7f2244d634f2c2f1afe7bdd4
shalini rajput <rajputshalini8744@gmail.com>
2025-09-14 23:45:05 +0530

    refactor: move route logic to controllers and add jwtAuth middleware for verification

--- backend/routes/counsellorRoute.js
+++ backend/routes/counsellorRoute.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const { getAssignedUser } = require("../controller/counsellorController");
 const router = express.Router();
+const jwtAuth = require("../middleware/auth");
 
-router.get("/counsellor/getUser", (req, res) => {
-    res.send("get assigned user/student");
-})
+router.use(jwtAuth);
+
+router.get("/getUser", getAssignedUser)
 
 module.exports = router;
