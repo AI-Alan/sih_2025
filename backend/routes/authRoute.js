fb2b7df9dad21ed8e8560ef0817c8b2f32ce7b91
shalini rajput <rajputshalini8744@gmail.com>
2025-09-14 23:53:17 +0530

    chore: remove unused jwtAuth middleware

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -2,9 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { registerValidator, validateLogin } = require("../middleware/validator");
 const { login, signUp, logout, resetPassword } = require("../controller/authController");
-const jwtAuth = require("../middleware/auth");
-
-router.use(jwtAuth);
 
 router.post("/signUp", registerValidator, signUp)
 
