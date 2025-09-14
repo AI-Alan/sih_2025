bb66060d7237afa0ce7eed7467daafbe332617e2
shalini rajput <rajputshalini8744@gmail.com>
2025-09-15 00:06:46 +0530

    refactor: remove role check and rename file

--- backend/middleware/validator.js
+++ backend/middleware/validator.js
@@ -6,11 +6,13 @@ exports.registerValidator = [
     check("email", "email is required").isEmail().normalizeEmail({
         gmail_remove_dots: true
     }),
-    check("role", "Role is required").not().isEmpty()
 ]
 
-module.exports.validateLogin = [
+exports.validateLogin = [
   check("username", "username is required").not().isEmpty(),
   check("password", "password is required").not().isEmpty().trim()
-
 ];
+
+exports.validateNewPassword = [
+  
+]
