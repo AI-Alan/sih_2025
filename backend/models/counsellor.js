fa79baab8cf61e8cfbe988010b3b8d6f60f48a76
shalini rajput <rajputshalini8744@gmail.com>
2025-09-24 19:25:22 +0530

    add counsellor model

---
+++ backend/models/counsellor.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const counsellorSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    specialization: {
+        type: String,
+        required: true
+    },
+    experience: {
+        type: Number,
+        required: true
+    },
+    availability: {
+        type: [String],
+        required: true
+    }
+},{
+    timestamps: true
+}
+)
+
+const Counsellor = mongoose.model("Counsellor", counsellorSchema);
+export default Counsellor;
